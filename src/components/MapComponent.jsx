40a7646862f8620424ae520072f381da0303331e
Candice <candice@aytech.ca>
2019-09-26 15:03:56 -0700

    Added addMarker handler along with Vancouver coords

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -20,8 +20,8 @@ class Map extends Component {
   componentDidMount() {
     // Create map L.map() returns a map object
     const map = L.map('map', {
-      center: [49.8419, 24.0315],
-      zoom: 16,
+      center: [49.25, -123.12],
+      zoom: 13,
       layers: [
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -29,9 +29,21 @@ class Map extends Component {
       ]
     });
 
+    // TODO: Remove when interactivity is added, this is just for PoC
+    L.marker([49.25, -123.12]).addTo(map);
+
     this.setState({
       map,
     });
+  }
+
+  // TODO: Implement addMarker in DSL
+  handleAddMarker = () => {
+    const { map } = this.state;
+    // TODO: coords should be passed as a parameter
+    const coords = [49.25, -123.12];
+
+    L.marker(coords).addTo(map);
   }
 
   render() {
