9ed5dac97ca08ec103e380520fe4a841bfe3254c
Patrick Hopkins <patrickdanielhopkins@gmail.com>
2019-10-10 21:07:16 -0700

    Made MapStore Singleton-esque. Added methods for adding stuff to map.

--- src/utils/MapStore.js
+++ src/utils/MapStore.js
@@ -1,12 +1,52 @@
+import L from 'leaflet';
 
-var mapStore = null;
+let instance = null;
 
-var setMap = function(map) {
-    mapStore = map;
-};
+class MapStore {
 
-var getMap = function() {
-    return mapStore;
-};
+    constructor() {}
 
-export default {getMap: getMap, setMap: setMap}
+    static getInstance() {
+        if (!instance) {
+            instance = new MapStore();
+        }
+        return instance;
+    }
+
+    getMap() {
+        return this.map;
+    }
+
+    setMap(map) {
+        this.map = map;
+    }
+
+    addMarker(latlon) {
+        L.marker(latlon).addTo(this.map)
+    }
+
+    addPolygon(latlons, color='blue', opacity=0.5) {
+        L.polygon(latlons, {
+            color: color,
+            fillOpacity: opacity
+        }).addTo(this.map)
+    }
+
+    addPolyline(latlons, color='green') {
+        L.polyline(latlons, {color: color}).addTo(this.map);
+    }
+
+    addCircle(latlon, color='red', opacity=0.5, radius=50) {
+        L.circle(latlon, {
+            radius: radius,
+            color: color,
+            fillOpacity: opacity
+        }).addTo(this.map);
+    }
+
+    addPopup(mapObject, text) {
+        mapObject.bindPopup(text).openPopup();
+    }
+}
+
+export default MapStore;
