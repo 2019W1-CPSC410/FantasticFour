3d7b1d3b5ac8384eddbe95217d0d1df5de2af348
Candice <candice@aytech.ca>
2019-10-12 15:49:14 -0700

    Added Program parse()

--- src/ast/Program.js
+++ src/ast/Program.js
@@ -1,17 +1,56 @@
 import MapStore from '../utils/MapStore';
 import VarStore from '../utils/VarStore';
-
-var statements = [];
+import Tokenizer from '../libs/Tokenizer';
+import Latlon from './Latlon';
+import Marker from './Marker';
+import Circle from './Circle';
+import Popup from './Popup';
+import Tooltip from './Tooltip';
+import Polygon from './Polygon';
+import Polyline from './Polyline';
+import Link from './Link';
+import Center from './Center';
 
 class Program {
-    constructor() {}
+    constructor() {
+        this.statements = [];
+    }
+
+    getSubStatement() {
+        switch (Tokenizer.checkToken()) {
+            case 'latlon':
+                return new Latlon();
+            case 'marker':
+                return new Marker();
+            case 'circle':
+                return new Circle();
+            case 'popup':
+                return new Popup();
+            case 'tooltip':
+                return new Tooltip();
+            case 'polygon':
+                return new Polygon();
+            case 'polyline':
+                return new Polyline();
+            case 'link':
+                return new Link();
+            case 'centered at':
+                return new Center();
+            default:
+                return null;
+        }
+    }
 
     parse () {
-
+        while (Tokenizer.moreTokens()) {
+            let s = this.getSubStatement();
+            s.parse();
+            this.statements.push(s);
+        }
     }
 
     evaluate() {
-        statements.forEach((statement) => {
+        this.statements.forEach((statement) => {
             statement.evalutate();
         })
     }
