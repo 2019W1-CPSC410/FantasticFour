38af2ae8c6d8f4e599cbc592ee39d9693508d9ab
Candice <candice@aytech.ca>
2019-10-13 20:47:03 -0700

    Fixed Link bug where polyline cannot be added  and renamed variables

--- src/ast/Link.js
+++ src/ast/Link.js
@@ -6,29 +6,29 @@ let tokenizer;
 
 class Link {
     constructor() {
-        this.first_name = '';
-        this.second_name = '';
+        this.first_location = '';
+        this.second_location = '';
         tokenizer = Tokenizer.getTokenizer();
     }
 
     parse () {
         tokenizer.getAndCheckNext('link');
-        this.first_name = tokenizer.getNext();
+        this.first_location = tokenizer.getNext(); // variable
         tokenizer.getAndCheckNext('with');
-        this.second_name = tokenizer.getNext();
+        this.second_location = tokenizer.getNext(); // variable
     }
 
     evaluate() {
         let mapStore = MapStore.getInstance();
-        let first_object = VarStore.getMapObject(this.first_name);
-        let second_object = VarStore.getMapObject(this.second_name);
+        let first_object = VarStore.getMapObject(this.first_location);
+        let second_object = VarStore.getMapObject(this.second_location);
 
         // TODO: type check that links are only between circles and markers (see class example)
         let first_latlon = first_object.getLatLng();
         let second_latlon = second_object.getLatLng();
 
         // TODO: could add options to link since its a polyline, for now only default color.
-        let marker = mapStore.addPolyLine([first_latlon, second_latlon]);
+        let marker = mapStore.addPolyline([first_latlon, second_latlon]);
     }
 }
 
